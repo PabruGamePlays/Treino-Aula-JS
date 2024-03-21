cf7f7b93614c3da209b2b8f155da035f30944204
Pablo Miguel <pablo2003miguel0903@gmail.com>
2024-03-21 17:32:59 -0300

    atividade 23

--- Arrays_E_Objetos/js/script.js
+++ Arrays_E_Objetos/js/script.js
@@ -285,4 +285,18 @@ console.log(palavra.repeat(5));*/
 //Fim de operações para String
 
 
-//23 - Rest Parameters
+//23 - Rest Parameters/Rest Operator
+const somaInfinita = (...args) => {
+
+    let total = 0;
+
+    for(let i =0; i < args.length; i++){
+        total += args[i];
+    }
+
+    return total;
+}
+
+console.log(somaInfinita(1, 2, 3));
+
+console.log(somaInfinita(1, 20, 34, 420, 572, 1803));
