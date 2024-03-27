1c8c260cadcb94505c61318f077003c49f8aad5e
Pablo Miguel <pablo2003miguel0903@gmail.com>
2024-03-27 09:32:22 -0300

    finalização de poo

--- Orientação a Obj JS/js/script.js
+++ Orientação a Obj JS/js/script.js
@@ -244,3 +244,31 @@ console.log(myPost.exibirTitulo);
 
 myPost.adicionarTags = "programação, javascript, JS";
 console.log(myPost);
+
+
+//14 - Herança
+class Mamifero{
+    constructor(patas){
+        this.patas = patas
+    }
+}
+
+class Lobo extends Mamifero{
+    constructor(patas, nome){
+        super(patas, patas)
+        this.nome = nome
+    }
+}
+
+const shark = new Lobo (4, "Shark")
+
+console.log(shark)
+
+
+//15 - instanceOf
+console.log(shark instanceof Lobo);
+console.log(Lobo instanceof Mamifero);
+
+console.log(new Lobo(4, "Teste") instanceof Mamifero);
+
+console.log(new Post("a", "b") instanceof Lobo);
